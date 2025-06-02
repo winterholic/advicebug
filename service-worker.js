69f1ae0135e3d5f805bd2a6087d1ae836b4a661c
winterholic <iuholic83@naver.com>
2025-06-03 04:27:03 +0900

    Feat : PWA앱으로 변환 시도

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'hunsuchung-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+}); 
